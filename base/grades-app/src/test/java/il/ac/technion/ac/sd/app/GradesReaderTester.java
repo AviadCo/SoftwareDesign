c5decef3e2db1ac0e08bb123d6ce2a7d2bcf3f0b
noam <noamyft@gmail.com>
2017-04-23 14:30:19 +0300

    GradesReaderTester: add testing for method getGrade.
    Signed-off-by: noam <noamyft@gmail.com>

--- base/grades-app/src/test/java/il/ac/technion/ac/sd/app/GradesReaderTester.java
+++ base/grades-app/src/test/java/il/ac/technion/ac/sd/app/GradesReaderTester.java
@@ -7,8 +7,11 @@ import java.util.OptionalInt;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import il.ac.technion.cs.sd.app.GradesInitializer;
 import il.ac.technion.cs.sd.app.GradesReader;
+import student.IStudentsDatabase;
 import student.Student;
 
 public class GradesReaderTester {
@@ -32,5 +35,49 @@ public class GradesReaderTester {
     	assertEquals(gradesReader.parseStudentGrade(Optional.of(new Student("123", 55))), OptionalInt.of(55));
     	assertEquals(gradesReader.parseStudentGrade(Optional.of(new Student("123", 63))), OptionalInt.of(63));
     	assertEquals(gradesReader.parseStudentGrade(Optional.of(new Student("264", 15))), OptionalInt.of(15));
+	}
+	
+	@Test
+	public void parseFindExistingGrade()
+	{    	
+    	IStudentsDatabase studentDBMock = Mockito.mock(IStudentsDatabase.class);
+    	
+    	Mockito.when(studentDBMock.findStudentByID("123")).thenReturn(Optional.of(new Student("123",45)));
+    	
+    	GradesReader gradesReadTemp = new GradesReader(studentDBMock);
+    	
+    	OptionalInt result = gradesReadTemp.getGrade("123");
+    	
+    	assertTrue(result.isPresent());
+    	assertEquals(45, result.getAsInt());
+	}
+	
+	@Test
+	public void parseFindNotExistingGrade()
+	{    	
+    	IStudentsDatabase studentDBMock = Mockito.mock(IStudentsDatabase.class);
+    	
+    	Mockito.when(studentDBMock.findStudentByID("123")).thenReturn(Optional.empty());
+    	
+    	GradesReader gradesReadTemp = new GradesReader(studentDBMock);
+    	
+    	OptionalInt result = gradesReadTemp.getGrade("123");
+    	
+    	assertFalse(result.isPresent());
+	}
+	
+	@Test
+	public void parseFindStudentByIDWithNull()
+	{    	
+    	IStudentsDatabase studentDBMock = Mockito.mock(IStudentsDatabase.class);
+    	
+    	Mockito.when(studentDBMock.findStudentByID(null)).thenReturn(Optional.of(new Student("123",45)));;
+    	
+    	GradesReader gradesReadTemp = new GradesReader(studentDBMock);
+    	
+    	OptionalInt result = gradesReadTemp.getGrade("123");
+    	
+    	assertFalse(result.isPresent());
+    	
 	}
 }
